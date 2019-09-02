7710c8be152e3230a261432774be7e870b76cdfc
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2019-09-02 23:26:46 +0900

    refactoring

--- lib/jekyll-typescript/hooks.rb
+++ lib/jekyll-typescript/hooks.rb
@@ -7,15 +7,15 @@ Jekyll::Hooks.register :site, :after_init do |site|
   config.hooks.each do |hook|
     container_name = hook["container"].to_sym
     event_name = hook["event"].to_sym
-    source_dir = hook["source_dir"] || config.default_source_dir
-    source_file = hook["source_file"]
-    handler = JekyllTypescript::Handler.new(config.get_ts_dir(source_dir),
-                                            config.get_build_dir(source_dir))
+    ts_dir_rel = hook["source_dir"] || config.default_source_dir
+    ts_rel_path = hook["source_file"]
+    handler = JekyllTypescript::Handler.new(config.get_ts_dir(ts_dir_rel),
+                                            config.get_build_dir(ts_dir_rel))
     if (container_name == :site) && (event_name == :after_init)
-      handler.run(source_file)
+      handler.run(ts_rel_path)
     else
       Jekyll::Hooks.register container_name, event_name do |container|
-        handler.run(source_file)
+        handler.run(ts_rel_path)
       end
     end
   end
