b72da31e10a4617d45cffc8fcfc4980ce8de9237
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2019-09-02 19:45:31 +0900

    setup rake application within ruby script

--- lib/jekyll-typescript-converter.rb
+++ lib/jekyll-typescript-converter.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 require "pathname"
+require "fileutils"
 require "json"
 require "rake"
 
@@ -28,16 +29,14 @@ module Jekyll
         ts_dir = site_source_path / config["source_dir"]
         # build target
         build_dir = site_source_path / config["build_dir"]
-        ENV["TSBUILD"] = build_dir.to_s
         target = build_dir / get_target_filename(data)
         # rake
         Dir.chdir(ts_dir)
-        puts "Running tsc (rake)..."
         # system("bundle exec rake")
-        Rake.with_application do |rake|
+        Rake.with_application do |rake_app|
           # Rake.with_application の定義 (in rake/rake_module.rb) のコメント参照
-          rake.load_rakefile
-          rake[target.to_s].invoke
+          setup(rake_app, build_dir)
+          rake_app[target.to_s].invoke
         end
         return target.read
       end
@@ -62,6 +61,35 @@ module Jekyll
           ext = ".js"
         end
         return data["source"].strip.sub(/\.ts$/, ext)
+      end
+
+      def setup(rake_app, build_dir)
+        if File.exist?("Rakefile")
+          puts "Setup rake from Rakefile"
+          ENV["TSBUILD"] = build_dir.to_s
+          rake_app.load_rakefile
+        else
+          puts "Setup rake within jekyll-typescript-converter"
+          tsfile_list = Dir["./**/*.ts"]
+
+          # generate .js file by tsc
+          jsfile_list = tsfile_list.map do |tsfile|
+            basename = tsfile.sub(/\.ts$/, ".js").sub(%r(^\./), "")
+            "#{build_dir}/#{basename}"
+          end
+
+          jsfile_list.each{|jsfile|
+            rake_app.define_task Rake::FileTask, {jsfile => tsfile_list} do |t|
+              puts "Creating #{jsfile} from #{tsfile_list}..."
+              `tsc --outDir #{build_dir}`
+            end
+            browserified_jsfile = jsfile.sub(/\.js$/, ".browserified.js")
+            rake_app.define_task Rake::FileTask, {browserified_jsfile => jsfile} do |t|
+              puts "Browserifying #{jsfile}..."
+              `browserify #{jsfile} -o #{browserified_jsfile}`
+            end
+          }
+        end
       end
     end
   end
