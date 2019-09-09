36132573252690d42bde4267c9c836dde5b10859
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2019-09-09 17:31:39 +0900

    read _data from javascript

--- test/_ts2/test.ts
+++ test/_ts2/test.ts
@@ -1,9 +1,16 @@
 import * as fs from 'fs';
 
+interface SiteObject {
+  // incomplete
+  site: {
+    data: any
+  }
+}
+
 function test(): void {
   console.log("Message from test!!!");
-  const jsonObject = JSON.parse(fs.readFileSync("./site.json", "utf8"));
-  console.log(jsonObject);
+  const jsonObject = JSON.parse(fs.readFileSync("./site.json", "utf8")) as SiteObject;
+  console.log(jsonObject.site.data);
 }
 
 test();
