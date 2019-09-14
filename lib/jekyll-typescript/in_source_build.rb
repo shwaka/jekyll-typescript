7e6c50d926270adddedfc0d1d1d3b7c3b761a7f3
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2019-09-14 23:57:20 +0900

    prevent to build twice in in-source build

--- lib/jekyll-typescript/in_source_build.rb
+++ lib/jekyll-typescript/in_source_build.rb
@@ -1,6 +1,22 @@
 # coding: utf-8
 require "jekyll-typescript/config"
 require "jekyll-typescript/handler"
+require "jekyll-watch"
+
+# in-source build creates .js files to the place where Watcher is watching
+# The following patch prevents these files to be watched
+module Jekyll
+  module Watcher
+    alias _orig__to_exclude to_exclude
+    def to_exclude(options)
+      config = JekyllTypescript::Config.new(options)
+      destinations = config.in_source_build.map do |build_info|
+        (config.site_source / build_info["destination"]).to_s
+      end
+      return _orig__to_exclude(options) + destinations
+    end
+  end
+end
 
 Jekyll::Hooks.register :site, :after_reset do |site|
   config = JekyllTypescript::Config.new(site.config)
